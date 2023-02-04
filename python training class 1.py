1d52323804e8982a94f2e9e75ce29a4c22e5cd9b
vickyhercules <123284873+vickyhercules@users.noreply.github.com>
2023-02-04 16:00:05 +0530

    Add files via upload

---
+++ python training class 1.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello")
+
+
+# In[2]:
+
+
+print("hi/nhello/nwelcome")
+
+
+# In[3]:
+
+
+print("hi\nhello\nwelcome")
+
+
+# In[4]:
+
+
+# hash is a single line comment 
+#print("hi")
+print("hello")
+print("welcome")
+
+
+# In[5]:
+
+
+"""
+jdkjj
+hjei
+huhe
+
+
+"""
+
+
+# In[6]:
+
+
+input("enter a value")
+
+
+# In[7]:
+
+
+a="hello welcome to today class"
+
+
+# In[8]:
+
+
+print(a)
+
+
+# In[9]:
+
+
+##variable name should not start with number
+##variable name should not have spcl charecter
+##fr ex a is a variable in above case. 
+## in python under score is not considered as a spcl charctr.
+
+
+# In[10]:
+
+
+a="test"
+print(type(a))
+
+
+# In[11]:
+
+
+print(isinstance(a,str))
+
+
+# In[14]:
+
+
+a="SachinTendulkar" #python is a case sensitive.
+print(len(a))#get lenght
+print(min(a))#get 
+print(max(a))
+print(sorted(a))
+
+
+# In[16]:
+
+
+print(a.upper())
+print(a.lower())
+print(a.swapcase())
+print(a.isupper())
+print(a.islower())
+print(a.isdigit())
+
+
+# In[17]:
+
+
+a="SachinTendulkar"
+
+
+# In[18]:
+
+
+print(a[5])
+
+
+# In[20]:
+
+
+#start : end :interval
+print(a[2:10])
+print(a[::])
+print(a[2:15:3])
+print(a[::-2])
+
+
+# In[21]:
+
+
+a="SachinTendulkar"
+
+
+# In[23]:
+
+
+print(a.index("a"))
+print(a.index("a",5))
+print(a.rindex("a"))
+
+
+# In[24]:
+
+
+print(a.find("a"))
+print(a.rfind("a"))
+
+
+# In[25]:
+
+
+print(a.count("a"))
+print(a.split("a"))
+print(a.replace("a","$"))
+
+
+# In[ ]:
+
+
+
+
